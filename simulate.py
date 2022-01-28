251d803aede4b81997189d752365f0e125574570
cerector <caitlin.rector@uvm.edu>
2022-01-28 17:38:12 -0500

    Starting work on simulation.

--- simulate.py
+++ simulate.py
@@ -1,7 +1,12 @@
+import pybullet_data
 import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("box.sdf")
 for x in range(0,1000):
     p.stepSimulation()
     time.sleep(1/60)
